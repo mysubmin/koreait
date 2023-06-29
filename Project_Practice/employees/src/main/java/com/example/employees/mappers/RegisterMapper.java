0aac4020cd566d775bf5bacba5807ae54b0505c6
mysubmin <134607089+mysubmin@users.noreply.github.com>
2023-06-30 08:44:37 +0900

    Update RegisterMapper.java

--- Project_Practice/employees/src/main/java/com/example/employees/mappers/RegisterMapper.java
+++ Project_Practice/employees/src/main/java/com/example/employees/mappers/RegisterMapper.java
@@ -1,6 +1,7 @@
 package com.example.employees.mappers;
 
-import com.example.employees.dto.RegisterDto;
+import com.example.employees.dto.DeptDto;
+import com.example.employees.dto.PosDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,6 +10,13 @@ import java.util.List;
 @Mapper
 public interface RegisterMapper {
     @Select("SELECT * FROM kor_dept ORDER BY kor_dept_code DESC")
-    List<RegisterDto> getDto();
+    List<DeptDto> getDept();
+
+    @Select("SELECT * FROM kor_pos WHERE kor_dept_code = #{selDeptValue}")
+    List<PosDto> getPos(String selDeptValue);
+
+    @Select("SELECT COUNT(*) FROM kor_employees WHERE kor_emp_email = #{email}")
+    int emailCheck(String email);
+
 
 }
