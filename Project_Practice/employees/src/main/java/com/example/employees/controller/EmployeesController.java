34aa614007fbbd56b10081d418749b7175383b9d
mysubmin <134607089+mysubmin@users.noreply.github.com>
2023-07-11 20:39:49 +0900

    Add files via upload

--- Project_Practice/employees/src/main/java/com/example/employees/controller/EmployeesController.java
+++ Project_Practice/employees/src/main/java/com/example/employees/controller/EmployeesController.java
@@ -4,16 +4,15 @@ package com.example.employees.controller;
 import com.example.employees.dto.EmployeeDto;
 import com.example.employees.mappers.EmployeeMapper;
 import com.example.employees.mappers.LevelMapper;
+import com.example.employees.mappers.ReigsterMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,12 +23,15 @@ import java.util.Map;
 @Controller
 public class EmployeesController {
 
-    private String UPLOAD_LOCATION = "D:\\koreait\\Java\\spring\\employees\\src\\main\\resources\\static\\upload\\";
+    private String UPLOAD_LOCATION = "D:\\koreait\\Java\\spring\\employees\\src\\main\\resources\\static\\upload";
 
     private EmployeeMapper employeeMapper;
 
     @Autowired
     private LevelMapper levelMapper;
+
+    @Autowired
+    private ReigsterMapper reigsterMapper;
 
     @Autowired
     public EmployeesController(EmployeeMapper employeeMapper) {
@@ -38,7 +40,9 @@ public class EmployeesController {
 
     @GetMapping("/admin/employees")
     public String getEmpList(Model model) {
+        model.addAttribute("dept", reigsterMapper.getDept());
         model.addAttribute("emp", employeeMapper.getEmpList());
+        model.addAttribute("level", employeeMapper.getLevel());
         return "admin/employees";
     }
 
@@ -47,7 +51,16 @@ public class EmployeesController {
     public Map<String, Object> deleteEmp(@RequestParam int korEmpId) {
         Map<String, Object> map = new HashMap<>();
         if( korEmpId > 0 ) {
-            employeeMapper.deleteEmp(korEmpId);
+            EmployeeDto edto = employeeMapper.getImageName(korEmpId); //image 파일
+
+            System.out.println(edto.getKorEmpImageName());
+            File file = new File(UPLOAD_LOCATION + "\\" + edto.getKorEmpImageName());
+            boolean b = file.delete();
+            if( b ) {
+                System.out.println("이미지 삭제 성공");
+            }
+
+            employeeMapper.deleteEmp(korEmpId); //db
             map.put("msg", "success");
         }
         return map;
@@ -90,6 +103,19 @@ public class EmployeesController {
 
         }catch(Exception ex) {
             ex.printStackTrace();
+        }
+        return map;
+    }
+
+    @GetMapping("/admin/employees/updateLevel")
+    @ResponseBody
+    public Map<String, Object> updateLevel(@ModelAttribute EmployeeDto employeeDto) {
+        Map<String, Object> map = new HashMap<>();
+
+        if(employeeDto.getKorEmpId() > 0 && employeeDto.getKorEmpLevel() > 0) {
+            //update query
+            employeeMapper.updateLevel(employeeDto);
+            map.put("msg", "success");
         }
         return map;
     }
--- Project_Practice/employees/src/main/java/com/example/employees/mappers/EmployeeMapper.java
+++ Project_Practice/employees/src/main/java/com/example/employees/mappers/EmployeeMapper.java
@@ -2,6 +2,7 @@ package com.example.employees.mappers;
 
 
 import com.example.employees.dto.EmployeeDto;
+import com.example.employees.dto.LevelDto;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -28,6 +29,16 @@ public interface EmployeeMapper {
             "kor_emp_image_size = #{korEmpImageSize} " +
             "WHERE kor_emp_id = #{korEmpId}")
     void fileUpload(EmployeeDto employeeDto);
+
+    @Select("SELECT kor_emp_image_name FROM kor_employees WHERE kor_emp_id = #{korEmpId}")
+    EmployeeDto getImageName(int korEmpId);
+
+    @Select("SELECT * FROM kor_level")
+    List<LevelDto> getLevel();
+
+    @Update("UPDATE kor_employees SET kor_emp_level = #{korEmpLevel} WHERE kor_emp_id = #{korEmpId}")
+    void updateLevel(EmployeeDto employeeDto);
+
 }
 
 
